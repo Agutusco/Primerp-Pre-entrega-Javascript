5974294b38c95deaf9b0a44754fbf6e39080ae19
Agustín Lombardi <agutusco@gmail.com>
2023-12-19 15:43:57 -0300

    Termino segunda pre-entrega

--- main.js
+++ main.js
@@ -1,62 +1,114 @@
 
-let bienvenida = alert("Bienvenido a multifruta")
 
-let precio1 = 1200 
-let precio2 = 900
-let precio3 = 1100
-let precio4 = 800
-let precio5 = 600
 
-let cantidad1 = 0
-let cantidad2 = 0
-let cantidad3 = 0
-let cantidad4 = 0
-let cantidad5 = 0
+const productos = [
+    {nombre:"Manzana" , tipo: "Fruta", stock: true, precio: 1200, cantidad:0},
+    {nombre:"Mandarina" , tipo: "Fruta", stock: true, precio: 900, cantidad:0},
+    {nombre:"Lechuga" , tipo: "Verdura", stock: false, precio: 1100, cantidad:0},
+    {nombre:"Pepino", tipo: "Verdura", stock: true, precio: 1400, cantidad:0},
+    {nombre:"Orégano", tipo: "Especias", stock: true, precio: 900, cantidad:0},
+    {nombre:"Mentas", tipo: "Especias", stock: false, precio: 700, cantidad:0}
+]
+class Pago {
+    constructor(medio,tarjeta, codigo, dni){
+        this.medio = medio
+        this.tarjeta = tarjeta
+        this.codigo = codigo
+        this.dni = dni 
+    }
+}
 
-let productos
-let suma
+let codigoSeg = ""
+function code() {
+    while (codigoSeg.length !== 4) {
+        codigoSeg = prompt("Ingresa el código de seguridad (4 dígitos)")
+    }
+    
+    return codigoSeg
+    
+}
+let dniSecurity = ""
+function dniSeg() {
+    while(dniSecurity.length !== 8){
+        dniSecurity = prompt("Ingresa tu DNI")
+    }
+    return dniSecurity
+}
+
+let bienvenida = alert("Bienvenido a multifruta, estos son nuestros productos")
+
+let productosElegidos = []
+let preguntaProds
+let quantity
+let suma = 0
+let stocks
+
+stocks = productos.filter(elm => elm.stock === false )
+
+stocks.forEach((producto) => {
+    stocks = stocks.slice(0, -2) + `, ${producto.nombre}`
+})
+
+alert(`Antes de iniciar, queriamos contarle que no contamos con los siguientes productos por falta de stock: ${stocks} `)
+
+
+
 do{
     
-    productos = prompt('\n¿Que productos quiere? Atención, solo vendemos por KILO. Si no desea comprar ahora, presiona 0\n Naranjas ($1200), Frutillas ($900),\n bananas ($1100),\n Mandarinas ($800),\n Peras ($600) ').toLowerCase()
-
-    if(productos === ""){
+    let mostarProds = productos.map((elm) => ` ${elm.nombre}: $${elm.precio}.
+    `)
+    preguntaProds = prompt('¿Que productos quiere?' + mostarProds.join("")).toLowerCase()
+    
+    if(preguntaProds === ""){
         alert("No seleccionaste ningún producto")
-    }else if (productos === "0"){
+    }else if (preguntaProds === "0"){
         alert("Hasta la próxima!!")
         break
     }else{
-        switch (productos){
-            case "naranjas":
-                cantidad1 = verificarNum("¿Cuantos kilos de naranja quiere?")
-                console.log("naranjas $1200");
+        switch (preguntaProds){
+            case "manzanas":
+                quantity = verificarNum("¿Cuantos kilos de manzana quiere?")
+                productosElegidos.push(preguntaProds)
+                suma += quantity * productos[0].precio
+                console.log("manzana $1200");
                 if(!confirm('¿Quiere seguir comprando?')) {
                     break
                 }
                 continue
-            case "frutillas":
-                cantidad2 = verificarNum("¿Cuantos kilos de frutillas quiere?")
-                console.log("frutillas $900");
+            case "mandarina":
+                quantity = verificarNum("¿Cuantos kilos de mandarina quiere?")
+                productosElegidos.push(preguntaProds)
+                suma += quantity * productos[1].precio
+                console.log("mandarina $900");
                 if(!confirm('¿Quiere seguir comprando?')) {
                     break
                 }
                 continue
-            case "bananas":
-                console.log("bananas $1100");
-                cantidad3 = verificarNum("¿Cuantos kilos de banana quiere?")
+            case "lechuga":
+                alert("Lo siento, no tenemos este producto")
                 if(!confirm('¿Quiere seguir comprando?')) {
                     break
                 }
                 continue
-            case "mandarinas":
-                cantidad4 = verificarNum("¿Cuantos kilos de mandarina quiere?")
-                console.log("mandarinas $800");
+            case "pepino":
+                quantity = verificarNum("¿Cuantos kilos de pepinos quiere?")
+                productosElegidos.push(preguntaProds)
+                suma += quantity * productos[3].precio
+                console.log("pepino $1400");
                 if(!confirm('¿Quiere seguir comprando?')) {
                     break
                 }
                 continue
-            case "peras":
-                cantidad5 = verificarNum("¿Cuantos kilos de peras quiere?")
-                console.log("peras 600");
+            case "oregano":
+                quantity = verificarNum("¿Cuantos kilos de oregano quiere?")
+                productosElegidos.push(preguntaProds)
+                suma += quantity * productos[4].precio
+                console.log("oregano 900");
+                if(!confirm('¿Quiere seguir comprando?')) {
+                    break
+                }
+            case "menta":
+                alert("Lo siento, no tenemos este producto")
                 if(!confirm('¿Quiere seguir comprando?')) {
                     break
                 }
@@ -66,23 +118,15 @@ do{
     }
     }
 
-    if (productos != 0) {
-        alert("Tu monto por ahora es de " + sumaTodo())
+    if (preguntaProds != 0) {
         alert("Su precio final es de " + descuentos())
         break
     }
-}while(productos !== "")
+}while(preguntaProds !== "")
+console.log(productosElegidos);
+console.log(suma);
 
 
-
-
-
-
-
-function sumaTodo() {
-    suma = (precio1 * cantidad1) + (precio2 * cantidad2) + (precio3 * cantidad3) + (precio4 * cantidad4) + (precio5 * cantidad5)
-    return suma
-}
 
 function verificarNum(str) {
     let num = Number(prompt(str))
@@ -95,23 +139,52 @@ function verificarNum(str) {
 
 }
 
+
 function descuentos() {
-        let envio = prompt("¿Lo retiras en tienda? Habrá una suma de $300 si lo llevamos a su domicilio", "Si/No").toLowerCase()
-        if (envio === "si") {
-            suma = suma
-        }else if(envio === "no"){
-            let domicilio = prompt("¿Cuál es su domicilio?")
-            suma += 300
-            alert("Perfecto, llevaremos su pedido a " + domicilio)
-        }
-        
-        let pago = prompt("¿Pagas en efectivo? Hay un descuento de $700 con este medio de pago", "Si/No").toLowerCase()
+
+
+    let envio = prompt("¿Lo retiras en tienda? Habrá una suma de $300 si lo llevamos a su domicilio", "Si/No").toLowerCase()
+    if (envio === "si") {
+        suma = suma
+    }else if(envio === "no"){
+        let domicilio = prompt("¿Cuál es su domicilio?")
+        suma += 300
+        alert("Perfecto, llevaremos su pedido a " + domicilio)
+    }
     
-        if (pago === "no") {
-            suma = suma
-        }else if (pago === "si"){
+        let medioPago = prompt(`¿Cual sera tu medio de pago?
+        Tarjeta Santander 10% OFF
+        Efectivo $700 OFF
+        Tarjeta Frances
+        Mercado Pago`).toLowerCase()
+        
+        if (medioPago === "santander" || medioPago === "tarjeta santander") {
+            const santander = new Pago("Tarjeta de Crédito", "Tarjeta Santander",code(), dniSeg())
+            console.log("si");
+            suma = suma * 0.9
+            alert(`Datos de su compra: 
+            productos: ${preguntaProds}. Usted llevo ${quantity} kilo/s
+            Datos de su pago: ${santander.medio}, ${santander.tarjeta}. Su código de seguridad es: ${santander.codigo} y su DNI es: ${santander.dni}`)
+        }else if (medioPago === "efectivo"){
+            const efectivo = new Pago("Efectivo", "", "", "")
             suma -= 700
+            alert(`Datos de su compra: 
+            productos: ${preguntaProds}. Usted llevo ${quantity} kilo/s
+            Datos de su pago: ${efectivo.medio}`)
+        }else if(medioPago === "frances" || medioPago === "tarjeta frances"){
+            const frances = new Pago("Tarjeta de crédito", "Tarjeta Francés", code(), dniSeg())
+            suma = suma
+            alert(`Datos de su compra: 
+            productos: ${preguntaProds}. Usted llevo ${quantity} kilo/s
+            Datos de su pago: ${frances.medio}, ${frances.tarjeta}. Su código de seguridad es: ${frances.codigo} y su DNI es: ${frances.dni}`)
+        }else if (medioPago === "mercado pago"){
+            const mp = new Pago("Mercado Pago", "", "", "")
+            suma = suma
+            alert(`Datos de su compra: 
+            productos: ${preguntaProds}. Usted llevo ${quantity} kilo/s
+            Datos de su pago: ${mp.medio}`)
         }
     
         return suma
 }
+
